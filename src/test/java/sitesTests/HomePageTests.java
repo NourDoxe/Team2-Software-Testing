b7db43186962216c02b463c97210d8f731ea8940
Nour <nourdoxe@gmail.com>
2025-05-03 04:42:20 +0300

    Add files via upload

--- src/test/java/sitesTests/HomePageTests.java
+++ src/test/java/sitesTests/HomePageTests.java
@@ -1,6 +1,6 @@
 package sitesTests;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
