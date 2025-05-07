3212a428426b9de46a71603f67ae5af88fd03703
Nour <nourdoxe@gmail.com>
2025-05-07 08:58:30 +0300

    ..

--- src/test/java/sitesTests/BaseTest.java
+++ src/test/java/sitesTests/BaseTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -20,16 +19,16 @@ public class BaseTest {
 
     @BeforeClass
     public void setup(){
-        //driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
-         EdgeOptions options = new EdgeOptions();
+        /* EdgeOptions options = new EdgeOptions();
         options.addArguments("--incognito");
-        driver = new EdgeDriver(options);
+        driver = new EdgeDriver(options); */
 
 
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
     }
     //screenshot
     public static void takeScreenShot (WebDriver driver , String filePath ){
